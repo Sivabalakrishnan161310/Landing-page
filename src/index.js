cc7c9bd78aac1021dc1ed6f3115ec10a6652eb34
Siva1611 <71762131048@cit.edu.in>
2024-06-09 16:00:58 +0530

    web

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Chat from './Chat';
-import reportWebVitals from './reportWebVitals';
 
 function Main() {
   const [showBot, setShowBot] = useState(false);
